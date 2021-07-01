49ca959abf38bcfd36791fc520fdce7a3cc94467
Andrey <cattalin.htc@gmail.com>
2021-07-01 12:18:13 +0200

    --wip-- [skip ci]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDaHLMlToAWIg5DcLD5ZN5PIYWzlU6wSRc",
+    authDomain: "demoproject-1f866.firebaseapp.com",
+    databaseURL: "https://demoproject-1f866.firebaseio.com",
+    projectId: "demoproject-1f866",
+    storageBucket: "demoproject-1f866.appspot.com",
+    messagingSenderId: "331647175493",
+    appId: "1:331647175493:web:e10520fe3a970fd6434c2c"
+  }
 };
